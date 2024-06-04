be8d1c768a4208c0b0d1d49dd5943d35d1064416
Yajat Pathak <yajatpathak@proton.me>
2024-06-04 21:15:39 +0530

    Made grid and navbar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Mauve
+# Mauve
 
 Mini project developed using React with TypeScript and Chakra-UI. <br />
 Application acts as a Video Game library in which games can be filtered and searched for. <br />
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <></>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        md: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="md">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+      <GridItem area="main">Main</GridItem>
+    </Grid>
+  );
 }
 
 export default App;
