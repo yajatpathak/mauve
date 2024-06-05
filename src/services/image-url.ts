f9f940704e610ddca354f67c00a8620caba9451f
Yajat Pathak <yajatpathak@proton.me>
2024-06-06 01:14:03 +0530

    added placeholder image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import placeholder from "../assets/placeholder/placeholder.png";
+
 function getCroppedImageUrl(url: string | null) {
-  if (url === null) return "";
+  if (url === null) return placeholder;
   let target = "media/";
   let index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
